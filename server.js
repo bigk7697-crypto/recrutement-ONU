2fd75b6ede19cb484deb08f755cdfee1d19f14e1
Alexandre Dubois <alex.dubois@email.com>
2026-07-07 16:26:22 +0200

    Force IPv4 for Supabase connection to fix ENETUNREACH on Render

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
